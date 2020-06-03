bbd26ca1295e754b18690f59967b91c3b7bcd300
Sameer Sinha <48231529+sameer882000@users.noreply.github.com>
2020-06-03 10:16:31 +0530

    first

---
+++ resource/js/script.js
@@ -0,0 +1,86 @@
+$(document).ready(function()
+{
+  // for sticky naigation
+
+
+  // Scroll on button-khud code likha
+
+  $('.js--scroll-to-plans').click(function () {
+     $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
+  });
+
+  $('.js--scroll-to-start').click(function () {
+     $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+  });
+
+   // Navigation scroll-kisi aur ka liya(jQuery snippet)
+   $(function() {
+     $('a[href*=#]:not([href=#])').click(function() {
+       if (loclocation.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostnameation.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+         var target = $(this.hash);
+         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+         if (target.length) {
+           $('html,body').animate({
+             scrollTop: target.offset().top
+           }, 1000);
+           return false;
+         }
+       }
+     });
+   });
+
+   // Animations on scroll
+   $('.js--wp-1').waypoint(function(direction) {
+       $('.js--wp-1').addClass('animated fadeIn');
+   }, {
+       offset: '50%'
+   });
+
+   $('.js--wp-2').waypoint(function(direction) {
+       $('.js--wp-2').addClass('animated fadeInUp');
+   }, {
+       offset: '50%'
+   });
+
+   $('.js--wp-3').waypoint(function(direction) {
+       $('.js--wp-3').addClass('animated fadeIn');
+   }, {
+       offset: '50%'
+   });
+
+   $('.js--wp-4').waypoint(function(direction) {
+       $('.js--wp-4').addClass('animated pulse');
+   }, {
+       offset: '50%'
+   });
+
+   /* Mobile navigation */
+   $('.js--nav-icon').click(function() {
+       var nav = $('.js--main-nav');
+       var icon = $('.js--nav-icon i');
+
+       nav.slideToggle(200);
+
+       if (icon.hasClass('ion-ios-person')) {
+           icon.addClass('ion-close-round');
+           icon.removeClass('ion-ios-person');
+       } else {
+           icon.addClass('ion-ios-person');
+           icon.removeClass('ion-close-round');
+       }
+   });
+
+
+
+
+
+
+
+
+
+
+  // practice
+  // $('h1').click(function(){
+  //   $(this).css('background-color','rgb(138, 62, 159)');
+  // });
+});
